f16608d4ece33605861c0abb560655e7159dcb9d
Ashu <ashu@techcertain.com>
2022-03-21 23:05:49 +1300

    //programme Report

---
+++ DealEngine.Domain.Entities/ProgrammeReports.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DealEngine.Domain.Entities.Abstracts;
+
+namespace DealEngine.Domain.Entities
+{
+    public class ProgrammeReports : EntityBase
+    {
+        public ProgrammeReports() : base(null)
+        {
+
+        }
+
+        public ProgrammeReports(User createdBy) : base(createdBy)
+        {
+            
+        }
+        public virtual string progreportsName { get; set; }
+        public virtual string progreportsValue { get; set; }
+        public virtual string progreportsDescription { get; set; }
+        public virtual IList<Programme> programme { get; set; }
+       
+    }
+
+}
---
+++ DealEngine.Services.Impl/ProgrammeReportsService.cs
@@ -0,0 +1,37 @@
+﻿using NHibernate.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DealEngine.Domain.Entities;
+using DealEngine.Infrastructure.FluentNHibernate;
+using DealEngine.Services.Interfaces;
+using System.Linq.Dynamic.Core;
+
+namespace DealEngine.Services.Impl
+{
+    public class ProgrammeReportsService : IProgrammeReportsService
+    {        
+        IMapperSession<ProgrammeReports> _programmeReports;
+
+        public ProgrammeReportsService( IMapperSession<ProgrammeReports> programmeReports)
+        {
+            _programmeReports = programmeReports;
+        }
+
+       
+
+        public async Task<List<ProgrammeReports>> GetAllProgrammeReports()
+        {
+           
+           var allProgrammeReports = await _programmeReports.FindAll().ToListAsync();
+           
+            return allProgrammeReports;
+        }
+
+        public async Task<ProgrammeReports> GetProgrammeReportsById(Guid Id)
+        {
+            return await _programmeReports.GetByIdAsync(Id);
+        }
+
+    }
+}
