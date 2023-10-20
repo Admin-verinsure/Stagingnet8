d4ab54eb1d99b36120310449c79c961cf6379126
Ashu <ashu@techcertain.com>
2023-10-20 15:47:03 +1300

    //

--- DealEngine.WebUI/Program.cs
+++ DealEngine.WebUI/Program.cs
@@ -1,24 +1,13 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using System;
 
 namespace DealEngine.WebUI
 {
     public static class Program
     {
-       
-        public static void Main(string[] args) =>CreateWebHostBuilder(args).Build().Run();
 
-        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-          WebHost.CreateDefaultBuilder(args)
-         .UseStartup<Startup>()
-         .UseKestrel(options =>
-         {
-             options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10); 
-                                                                         
-             options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(10); 
+        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
 
-
-         });
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
