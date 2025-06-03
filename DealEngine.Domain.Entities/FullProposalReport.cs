4cdf6111f2d886dc53b6dbb23972613da6e6ace9
Ashu Chauhan <ashu@techcertain.com>
2025-06-03 23:03:45 +1200

    //

--- DealEngine.Domain.Entities/FullProposalReport.cs
+++ DealEngine.Domain.Entities/FullProposalReport.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using DealEngine.Domain.Entities.Abstracts;
 using Newtonsoft.Json;
+using static NHibernate.Engine.Query.CallableParser;
 
 namespace DealEngine.Domain.Entities
 {
@@ -14,29 +15,54 @@ namespace DealEngine.Domain.Entities
 
         protected FullProposalReport() : base(null) { }
 
-        public virtual string DeclaringOfficer { get; set; }
-        public virtual string Organisations { get; set; }
+        public virtual string SubmissionReference { get; set; }
 
-        public virtual string Locations { get; set; }
-        public virtual string TerritoriesPercentage { get; set; }      
-        public virtual decimal ActualCommissionLastYear { get; set; }
-        public virtual decimal EstimatedCommissionCurrentYear { get; set; }
-        public virtual decimal EstimatedCommissionNextYear { get; set; }
-        public virtual string ActivitiesPercentageCurrentYear { get; set; }
-        public virtual string CoverClubAssets { get; set; }
-        public virtual string AnyClubAssetOver15000 { get; set; }
-        public virtual List<string> ScheduledAssetsOver15000 { get; set; }
-        public virtual string ClubRealEstate { get; set; }
-        public virtual string ClubMotorVehicle { get; set; }
-        public virtual string lossExperience5000 { get; set; }
-        public virtual string ClaimWithdrawn { get; set; }
-        public virtual string InsuranceIssues { get; set; }
-        public virtual string CriminalOffense { get; set; }
-        public virtual string Bankruptcy { get; set; }
-        public virtual List<string> Buildings { get; set; }
-        public virtual string GeneralLiability { get; set; }
-        public virtual string Youthprogrammedetails { get; set; }
-        public virtual string Documents { get; set; }
+
+        public virtual string EglobalClientNumber { get; set; }
+        public virtual string EglobalBranch { get; set; }
+        public virtual decimal NameofApplicant { get; set; }
+        public virtual decimal SubsidiaryCompanies { get; set; }
+        public virtual decimal PhoneNumber { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string PostalAddress { get; set; }
+
+        public virtual string NumberofYearsBusiness { get; set; }
+        public virtual string PeriodofInsuranceFrom { get; set; }
+        public virtual decimal PeriodofInsuranceTo { get; set; }
+        public virtual decimal GeneralLiabilityLimit { get; set; }
+        public virtual decimal StatutoryLiabilityLimit { get; set; }
+        public virtual string EmployersLiabilityLimit { get; set; }
+        public virtual string RetroactiveDate { get; set; }
+
+        public virtual string BusinessoutsideofNewZealand { get; set; }
+
+        public virtual string NumberEmployeesinNZ { get; set; }
+        public virtual decimal NumberlocationsNZ { get; set; }
+        public virtual decimal DescriptionActivity { get; set; }
+        public virtual decimal NameInsured { get; set; }
+        public virtual string CurrentYearTurnover { get; set; }
+        public virtual string NextYearTurnover { get; set; }
+
+        public virtual string jointventureactivites { get; set; }
+        public virtual string contructionactivites { get; set; }
+
+        public virtual string ApplyInsolvencyExclusion { get; set; }
+        public virtual string Businessisabletopay { get; set; }
+
+
+        public virtual decimal CurrentAssets { get; set; }
+        public virtual decimal TotalAssets { get; set; }
+        public virtual decimal CurrentLiabilities { get; set; }
+        public virtual decimal TotalLiabilities { get; set; }
+
+
+        public virtual decimal anydirectorhanydirector { get; set; }
+        public virtual decimal anydirectorhanydirector5yr { get; set; }
+        public virtual decimal Declinedrenewpolicy  { get; set; }
+		public virtual decimal withdrawnaclaim { get; set; }
+        public virtual string declinedanyclaim { get; set; }
+        public virtual string declaredbankrupt { get; set; }
+        public virtual string anycriminaloffence { get; set; }
 
 
         public virtual string Declaration { get; set; }
@@ -44,54 +70,84 @@ namespace DealEngine.Domain.Entities
 
 
         public static FullProposalReport Create(
-           string declaringOfficer,
-           string organisations,
-           string locations,
-           string territoriesPercentage,
-           decimal actualCommissionLastYear,
-           decimal estimatedCommissionCurrentYear,
-           decimal estimatedCommissionNextYear,
-           string activitiesPercentageCurrentYear,
-           string coverClubAssets,
-           string anyClubAssetOver15000,
-           List<string> scheduledAssetsOver15000,
-           string clubRealEstate,
-           string clubMotorVehicle,
-           string lossExperience5000,
-           string claimWithdrawn,
-           string insuranceIssues,
-           string criminalOffense,
-           string bankruptcy,
-           List<string> buildings,
-           string generalLiability,
-           string documents,
-           string declaration)
+           string submissionReference,
+    string eglobalClientNumber,
+    string eglobalBranch,
+    decimal nameofApplicant,
+    decimal subsidiaryCompanies,
+    decimal phoneNumber,
+    string email,
+    string postalAddress,
+    string numberOfYearsBusiness,
+    string periodOfInsuranceFrom,
+    decimal periodOfInsuranceTo,
+    decimal generalLiabilityLimit,
+    decimal statutoryLiabilityLimit,
+    string employersLiabilityLimit,
+    string retroactiveDate,
+    string businessoutsideofNewZealand,
+    string numberEmployeesinNZ,
+    decimal numberlocationsNZ,
+    decimal descriptionActivity,
+    decimal nameInsured,
+    string currentYearTurnover,
+    string nextYearTurnover,
+    string jointventureactivites,
+    string contructionactivites,
+    string applyInsolvencyExclusion,
+    string businessisabletopay,
+    decimal currentAssets,
+    decimal totalAssets,
+    decimal currentLiabilities,
+    decimal totalLiabilities,
+    decimal anydirectorhanydirector,
+    decimal anydirectorhanydirector5yr,
+    decimal declinedrenewpolicy,
+    decimal withdrawnaclaim,
+    string declinedanyclaim,
+    string declaredbankrupt,
+    string anycriminaloffence)
         {
             return new FullProposalReport
-            {
-                DeclaringOfficer = declaringOfficer,
-                Organisations = organisations,
-                Locations = locations,
-                TerritoriesPercentage = territoriesPercentage,
-                ActualCommissionLastYear = actualCommissionLastYear,
-                EstimatedCommissionCurrentYear = estimatedCommissionCurrentYear,
-                EstimatedCommissionNextYear = estimatedCommissionNextYear,
-                ActivitiesPercentageCurrentYear = activitiesPercentageCurrentYear,
-                CoverClubAssets = coverClubAssets,
-                AnyClubAssetOver15000 = anyClubAssetOver15000,
-                ScheduledAssetsOver15000 = scheduledAssetsOver15000,
-                ClubRealEstate = clubRealEstate,
-                ClubMotorVehicle = clubMotorVehicle,
-                lossExperience5000 = lossExperience5000,
-                ClaimWithdrawn = claimWithdrawn,
-                InsuranceIssues = insuranceIssues,
-                CriminalOffense = criminalOffense,
-                Bankruptcy = bankruptcy,
-                Buildings = buildings,
-                GeneralLiability = generalLiability,
-                Documents= documents,
-                Declaration = declaration
-            };
+    {
+        SubmissionReference             = submissionReference,
+        EglobalClientNumber             = eglobalClientNumber,
+        EglobalBranch                   = eglobalBranch,
+        NameofApplicant                 = nameofApplicant,
+        SubsidiaryCompanies             = subsidiaryCompanies,
+        PhoneNumber                     = phoneNumber,
+        Email                           = email,
+        PostalAddress                   = postalAddress,
+        NumberofYearsBusiness           = numberOfYearsBusiness,
+        PeriodofInsuranceFrom           = periodOfInsuranceFrom,
+        PeriodofInsuranceTo             = periodOfInsuranceTo,
+        GeneralLiabilityLimit           = generalLiabilityLimit,
+        StatutoryLiabilityLimit         = statutoryLiabilityLimit,
+        EmployersLiabilityLimit         = employersLiabilityLimit,
+        RetroactiveDate                 = retroactiveDate,
+        BusinessoutsideofNewZealand     = businessoutsideofNewZealand,
+        NumberEmployeesinNZ             = numberEmployeesinNZ,
+        NumberlocationsNZ               = numberlocationsNZ,
+        DescriptionActivity             = descriptionActivity,
+        NameInsured                     = nameInsured,
+        CurrentYearTurnover             = currentYearTurnover,
+        NextYearTurnover                = nextYearTurnover,
+        jointventureactivites           = jointventureactivites,
+        contructionactivites            = contructionactivites,
+        ApplyInsolvencyExclusion        = applyInsolvencyExclusion,
+        Businessisabletopay             = businessisabletopay,
+        CurrentAssets                   = currentAssets,
+        TotalAssets                     = totalAssets,
+        CurrentLiabilities              = currentLiabilities,
+        TotalLiabilities                = totalLiabilities,
+        anydirectorhanydirector         = anydirectorhanydirector,
+        anydirectorhanydirector5yr      = anydirectorhanydirector5yr,
+        Declinedrenewpolicy             = declinedrenewpolicy,
+        withdrawnaclaim                 = withdrawnaclaim,
+        declinedanyclaim                = declinedanyclaim,
+        declaredbankrupt                = declaredbankrupt,
+        anycriminaloffence              = anycriminaloffence
+    };
         }
 
 
